2e49aff76a553fff5cbaffb4eb0df85055942409
baptistequeyssac-VM <baptiste.queyssac@gmail.com>
2023-04-04 19:37:25 +0200

    Api\ArtistController : add function browse

--- src/Controller/Api/ArtistController.php
+++ src/Controller/Api/ArtistController.php
@@ -2,22 +2,48 @@
 
 namespace App\Controller\Api;
 
+use App\Repository\ArtistRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
+
 
 class ArtistController extends AbstractController
 {
     /**
-     * @Route("/api/artist", name="app_api_artist")
+     * @Route("/api/artist", name="app_api_artist", methods={"GET"})
+     * 
+     * @OA\Response(
+     *      response=200,
+     *      description="Returns all artists",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Artist::class, groups={"artist_browse"}))
+     *      )    
+     * )
      */
-    public function index(): JsonResponse
+
+    //* Return all artists
+    public function browse(ArtistRepository $artistRepository): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Api/ArtistController.php',
-        ]);
+        $allArtist = $artistRepository->findAll();
+
+        return $this->json(
+            $allArtist,
+            Response::HTTP_OK,
+            // Third's parameter is empty because we need to access fourth parameter
+            [],
+            [
+                "groups" =>
+                [
+                    "artist_browse"
+                ]
+            ]
+        );
+
     }
 }
 
-// test
