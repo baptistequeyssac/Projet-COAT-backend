43ab3518764a3db136c09c942527b89f36b27b02
Corentin-Lher <corentinlher29@gmail.com>
2023-04-04 17:18:51 +0200

    Create Api\OrganizerController

---
+++ src/Controller/Api/OrganizerController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class OrganizerController extends AbstractController
+{
+    /**
+     * @Route("/api/organizer", name="app_api_organizer")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/Api/OrganizerController.php',
+        ]);
+    }
+}
