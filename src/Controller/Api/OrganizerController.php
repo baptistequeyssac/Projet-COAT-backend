d5b816081808bc270a47dc8a520e0c60d3c1e8d6
Corentin-Lher <corentinlher29@gmail.com>
2023-04-04 19:52:13 +0200

    Api\OrganizerController : add function browse

--- src/Controller/Api/OrganizerController.php
+++ src/Controller/Api/OrganizerController.php
@@ -2,22 +2,46 @@
 
 namespace App\Controller\Api;
 
+use App\Repository\OrganizerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
+use Symfony\Component\HttpFoundation\Response;
 
 class OrganizerController extends AbstractController
 {
     /**
-     * @Route("/api/organizer", name="app_api_organizer")
+     * @Route("/api/organizer", name="app_api_organizer", methods={"GET"})
+     * 
+     *  @OA\Response(
+     *     response=200,
+     *     description="Returns all the organizer",
+     *     @OA\JsonContent(
+     *        type="array",
+     *        @OA\Items(ref=@Model(type=Organizer::class, groups={"organizer_browse"}))
+     *     )
+     * )
      */
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Api/OrganizerController.php',
-        ]);
+
+    //* Return all organizer
+    public function browse(OrganizerRepository $organizerRepository): JsonResponse
+    { 
+        $allOrganizer = $organizerRepository->findAll();
+        
+        return $this->json(
+            // object to be transmitted
+            $allOrganizer,
+            Response::HTTP_OK,
+            [],
+            [
+                "groups" => 
+              [
+                 "genre_browse"
+              ] 
+            ]
+        );
     }
 }
 
-//test 2
