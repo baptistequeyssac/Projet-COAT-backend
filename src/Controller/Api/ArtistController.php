a67c1b8fe6e3500679b65f04dc3e1a1c8b72b213
baptistequeyssac-VM <baptiste.queyssac@gmail.com>
2023-04-19 18:15:33 +0200

    Test add artist_id in entity user when add artist

--- src/Controller/Api/ArtistController.php
+++ src/Controller/Api/ArtistController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\Api;
 
 use App\Entity\Artist;
+use App\Entity\User;
 use App\Repository\ArtistRepository;
 use App\Repository\RegionRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -79,7 +81,8 @@ class ArtistController extends AbstractController
         Request $request,
         SerializerInterface $serializer,
         ArtistRepository $artistRepository,
-        ValidatorInterface $validator
+        ValidatorInterface $validator,
+        User $user
         )
      {
         $contentJson = $request->getContent();
@@ -108,6 +111,21 @@ class ArtistController extends AbstractController
                 Response::HTTP_UNPROCESSABLE_ENTITY
             );
         }
+
+        // ! TEST ! \\
+        // get user from artist
+        $user = $this->getUser();
+        if (!$user instanceof User) {
+            return $this->json(
+                'Utilisateur non trouvé',
+                //code 404
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        // set user for this artist
+        $user->setArtist($artistFromJson);
+        // ! TEST ! \\
 
         // persist + flush
         $artistRepository->add($artistFromJson, true);
