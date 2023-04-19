6568ed360e1ba31586ee0c6976d3bba85619da8b
baptistequeyssac-VM <baptiste.queyssac@gmail.com>
2023-04-19 15:26:20 +0200

    WIP  fix add image

--- src/Controller/Api/StockageController.php
+++ src/Controller/Api/StockageController.php
@@ -32,15 +32,15 @@ class StockageController extends AbstractController
     {
         $image = new Stockage();
 
-        $file = $request->files->get('image');
+        $file = $request->files->get('file');
         $fileName = uniqid() . '.' . $file->guessExtension();
 
         //
-        if (!$file) {
-            return new JsonResponse(
-                ['error' => 'No file was uploaded.']
-            );
-        }
+        // if (!$file) {
+        //     return new JsonResponse(
+        //         ['error' => 'No file was uploaded.']
+        //     );
+        // }
 
         try {
             $file->move(
