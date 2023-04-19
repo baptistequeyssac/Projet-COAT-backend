00ed9bf297bc121249e583d2dc91ffe5a4257dda
baptistequeyssac-VM <baptiste.queyssac@gmail.com>
2023-04-19 15:03:48 +0200

    WIP  fix add image

--- src/Controller/Api/StockageController.php
+++ src/Controller/Api/StockageController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class StockageController extends AbstractController
 {
