ae2b541728798fe3dc6ddc92e4467199c7a3b40e
baptistequeyssac-VM <baptiste.queyssac@gmail.com>
2023-04-20 15:59:13 +0200

    bug composer install

---
+++ src/Controller/Backoffice/HomeBackofficeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Backoffice;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeBackofficeController extends AbstractController
+{
+    /**
+     * @Route("/backoffice/home, name="app_backoffice_home_backoffice")
+     */
+    public function index(): Response
+    {
+        return $this->render('backoffice/home_backoffice/index.html.twig', [
+            'controller_name' => 'HomeBackofficeController',
+        ]);
+    }
+}
