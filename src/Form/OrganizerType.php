2f45f055170fbc0d59fa49a08f297a5af8afe78a
baptistequeyssac-VM <baptiste.queyssac@gmail.com>
2023-04-23 20:42:18 +0200

    PRE-MERGE

--- src/Form/OrganizerType.php
+++ src/Form/OrganizerType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Organizer;
+use App\Entity\Region;
+use App\Entity\Status;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,13 +22,17 @@ class OrganizerType extends AbstractType
             ->add('logo')
             ->add('email')
             ->add('phone')
-            ->add('createdAt')
-            ->add('updatedAt')
             ->add('events')
             // ->add('user')
             ->add('artist')
-            // ->add('status')
-            // ->add('region')
+            ->add('status', EntityType::class, [
+                'class' => Status::class,
+                'choice_label' => 'name'
+            ])
+            ->add('region', EntityType::class, [
+                'class' => Region::class,
+                'choice_label' => 'name',
+            ])
         ;
     }
 
