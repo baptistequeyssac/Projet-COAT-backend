1211e0ff427f91cbc3802e1d5ac6239d226e2314
baptistequeyssac-VM <baptiste.queyssac@gmail.com>
2023-04-23 18:54:33 +0200

    WIP form new artist

--- src/Form/ArtistType.php
+++ src/Form/ArtistType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Artist;
+use App\Entity\Region;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -21,13 +24,16 @@ class ArtistType extends AbstractType
             ->add('email')
             ->add('phone')
             ->add('address')
-            ->add('createdAt')
-            ->add('updatedAt')
+            // ->add('createdAt')
+            // ->add('updatedAt')
             ->add('category')
             ->add('events')
             // ->add('user')
             ->add('organizers')
-            // ->add('region')
+            ->add('region', EntityType::class, [
+                'class' => Region::class,
+                'choice_label' => 'name',
+            ])
         ;
     }
 
